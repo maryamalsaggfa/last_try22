4ff5b0fb029ac9c861e41ca98b943b7785818a88
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-05 02:50:26 -0700

    Initial Commit

---
+++ last_try22/last_try22App.swift
@@ -0,0 +1,17 @@
+//
+//  last_try22App.swift
+//  last_try22
+//
+//  Created by maryam on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct last_try22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
